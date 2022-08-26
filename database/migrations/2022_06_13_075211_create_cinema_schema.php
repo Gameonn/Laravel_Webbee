7026f92a12e7baa82b63b6e7375ebbbb6677699b
Gameonn <jindal.ankit89@gmail.com>
2022-08-26 18:31:35 +0530

    Add cinema schema

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -26,7 +26,8 @@ class CreateCinemaSchema extends Migration
 
      **Pricing**
      * As a cinema owner I want to get paid differently per show
-     * As a cinema owner I want to give different seat types a percentage premium, for example 50 % more for vip seat
+     * As a cinema owner I want to give different seat types a percentage premium, 
+     * for example 50 % more for vip seat
 
      **Seating**
      * As a user I want to book a seat
@@ -37,7 +38,106 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        // create movies table
+        Schema::create('movies', function(Blueprint $table)
+		{
+			$table->integer('movie_id');
+			$table->integer('movie_title');
+			$table->string('description');
+			$table->string('genre');
+			$table->timestamp('duration');
+
+			$table->primary('movie_id');
+		});
+
+        // create cinemas table
+        Schema::create('cinemas', function(Blueprint $table)
+		{
+			$table->integer('cinema_id');
+			$table->string('city');
+
+			$table->primary('cinema_id');
+        });
+
+        // create halls table
+        Schema::create('halls', function(Blueprint $table)
+		{
+			$table->integer('hall_id');
+			$table->integer('cinema_id');
+			$table->string('name');
+			$table->string('address');
+			$table->integer('seat_count');
+
+			$table->primary('hall_id');
+            $table->foreign('cinema_id')->references('cinema_id')->on('cinemas');
+        });
+
+        // create users table
+        Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('mobile_number');
+			$table->enum('user_role', ['user', 'admin', 'manager'])->default('user');
+
+		});
+
+        // create shows table
+        Schema::create('shows', function(Blueprint $table)
+		{
+            $table->integer('show_id');
+            $table->integer('movie_id');
+			$table->integer('hall_id');
+			$table->integer('price');
+			$table->datetime('start_time');
+			$table->datetime('end_time');
+
+			$table->primary('show_id');
+            $table->foreign('movie_id')->references('movie_id')->on('movies');
+            $table->foreign('hall_id')->references('hall_id')->on('halls');
+        });
+        
+
+        // create bookings table
+        Schema::create('bookings', function(Blueprint $table)
+		{
+			$table->integer('booking_id');
+			$table->integer('show_id');
+			$table->integer('user_id');
+			$table->integer('number_of_seats');
+			$table->enum('status', ['Confirmed', 'Waiting', 'Reject', 'Cancelled']);
+            $table->timestamps();
+
+			$table->primary('booking_id');
+            $table->foreign('show_id')->references('show_id')->on('shows');
+        });
+
+        // create hall_seats table
+        Schema::create('hall_seats', function(Blueprint $table)
+		{
+			$table->integer('hall_seat_id');
+			$table->integer('hall_id');
+			$table->enum('seat_type', ['vip', 'super vip', 'normal'])->default('normal');
+			$table->integer('seat_number');
+
+			$table->primary('hall_seat_id');
+            $table->foreign('hall_id')->references('hall_id')->on('halls');
+        });
+
+        // create seats table
+        Schema::create('seats', function(Blueprint $table)
+		{
+			$table->integer('seat_id');
+			$table->integer('hall_seat_id');
+			$table->integer('show_id');
+			$table->integer('booking_id');
+			$table->integer('price');
+			$table->enum('status', ['occupied', 'idle']);
+
+			$table->primary('seat_id');
+            $table->foreign('hall_seat_id')->references('hall_seat_id')->on('hall_seats');
+            $table->foreign('show_id')->references('show_id')->on('shows');
+            $table->foreign('booking_id')->references('booking_id')->on('bookings');
+        });
+        
     }
 
     /**
@@ -47,5 +147,34 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        // disable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        // drop the movies table
+		Schema::dropIfExists('movies');
+        
+		// drop the shows table
+		Schema::dropIfExists('shows');
+        
+		// drop the cinemas table
+		Schema::dropIfExists('cinemas');
+        
+		// drop the bookings table
+		Schema::dropIfExists('bookings');
+        
+		// drop the users table
+		Schema::dropIfExists('users');
+        
+		// drop the halls table
+		Schema::dropIfExists('halls');
+        
+		// drop the halls table
+		Schema::dropIfExists('hall_seats');
+        
+		// drop the halls table
+		Schema::dropIfExists('seats');
+    
+        // enable foreign key checks
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
